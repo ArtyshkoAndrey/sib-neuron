fb5f1c653666360262b036e00d7d00b933bc7f3f
Fullion <artyshko.andrey@gmail.com>
2018-11-13 23:51:05 +0700

    Update resources/views/dashboard/templates/app.blade.php

--- resources/views/dashboard/templates/app.blade.php
+++ resources/views/dashboard/templates/app.blade.php
@@ -7,7 +7,8 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('dashboard/plugins/font-awesome/css/font-awesome.min.css')}}">
+  <!-- <link rel="stylesheet" href="{{asset('dashboard/plugins/font-awesome/css/font-awesome.min.css')}}"> -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
@@ -52,7 +53,7 @@
     <footer class="main-footer">
     <strong>Powered by - Fulliton</strong>
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 0.1.0 - betta
+      <b>Version</b> 0.1.0 - Бета
     </div>
   </footer>
 
