04d0c1fe8ab4aa04f32eef7a2fbed50f527b8af1
ArtyshkoAndrey <artyshko.andrey@gmail.com>
2018-11-16 16:17:49 +0700

    Изменён размер окна с правилами

--- resources/views/teach.blade.php
+++ resources/views/teach.blade.php
@@ -67,8 +67,8 @@
                 </div>
             </div>
         </div>
-        <div class="modal fade bd-example-modal-lg" id="ruleModal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
+        <div class="modal fade bd-example-modal-md" id="ruleModal" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-md" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLongTitle">Правила обучения</h5>
